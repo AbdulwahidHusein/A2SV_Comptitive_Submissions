d1c215291cd5d8888ffa61039bb06520d4fc7c85
Abdi <inventorabdi@gmail.com>
2023-10-21 01:23:52 -0700

    some problems solved

--- String_problems/valid_palindrome.py
+++ String_problems/valid_palindrome.py
@@ -1,2 +1,15 @@
-def palind():
-    pass
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        def is_palin(s):
+            if len(s) <= 1:
+                return True
+            return s[1] == s[-1] and is_palin(s[1:-1])
+        cleaned = ''
+        for s in s:
+            if s.isalpha():
+                cleaned += s.lower()
+        if len(cleaned) == 1:
+            return True
+
+        return is_palin(cleaned)
+        
