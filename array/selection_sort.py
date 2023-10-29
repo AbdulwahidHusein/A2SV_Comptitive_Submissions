248ea8de9a21b938cee892a83ed4b78653f24453
Abdi <inventorabdi@gmail.com>
2023-10-29 09:48:44 -0700

    some problems solved

---
+++ array/selection_sort.py
@@ -0,0 +1,11 @@
+class Solution: 
+        # code here 
+    def selectionSort(self, arr,n):
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if arr[j] < arr[i]:
+                    temp = arr[i]
+                    arr[i] = arr[j]
+                    arr[j] = temp
+        s = " ".join([str(m) for m in arr])
+        return s
