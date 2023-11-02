4517ee0b70e2b550c5957c6a8d37b2ad798635cf
Abdi <inventorabdi@gmail.com>
2023-11-02 06:25:22 -0700

    File changes

---
+++ String_problems/valid_anagram.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        for idx in set(s):
+            if s.count(idx) != t.count(idx):
+                return False
+        return True
