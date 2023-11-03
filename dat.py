d3f771c8e2c8473ceb0d8be171c2b2db474c3c99
Abdi <inventorabdi@gmail.com>
2023-11-03 01:11:25 -0700

    File changes

--- dat.py
+++ dat.py
@@ -1,46 +1,15 @@
-# Python3 program to print 
-# primes smaller than n using
-# Sieve of Sundaram.
+arr = [1,3,3,3, 6, -6, 2, 9, 0, -6, 9, 15, 1, 5]
+target = 9
 
-# Prints all prime numbers smaller
-def SieveOfSundaram(n):
-	
-	# In general Sieve of Sundaram, 
-	# produces primes smaller
-	# than (2*x + 2) for a number 
-	# given number x. Since we want
-	# primes smaller than n, we 
-	# reduce n to half
-	nNew = int((n - 1) / 2);
+#1, 3, 6
+def find_elements(arr, k):
+    result = []
+    seen = set()
+    for num in arr:
+        diff = k - num
+        if diff in seen: 
+            result.append([num, diff])
+        seen.add(num)
 
-	# This array is used to separate 
-	# numbers of the form i+j+2ij
-	# from others where 1 <= i <= j
-	# Initialize all elements as not marked
-	marked = [0] * (nNew + 1);
-
-	# Main logic of Sundaram. Mark all 
-	# numbers of the form i + j + 2ij 
-	# as true where 1 <= i <= j
-	for i in range(1, nNew + 1):
-		j = i;
-		while((i + j + 2 * i * j) <= nNew):
-			marked[i + j + 2 * i * j] = 1;
-			j += 1;
-
-	# Since 2 is a prime number
-	if (n > 2):
-		print(2, end = " ")
-
-	# Print other primes. Remaining 
-	# primes are of the form 2*i + 1 
-	# such that marked[i] is false.
-	for i in range(1, nNew + 1):
-		if (marked[i] == 0):
-			print((2 * i + 1), end = " ")
-
-# Driver Code
-n = 20
-SieveOfSundaram(n)
-
-# This code is contributed by mits
+    return result
+print(find_elements(arr, target))
