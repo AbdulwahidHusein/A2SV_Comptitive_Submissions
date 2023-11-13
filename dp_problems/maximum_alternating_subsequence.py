cf821753de93c602ed98ea60bbd29b180c92e2b0
Abdi <inventorabdi@gmail.com>
2023-11-13 10:33:15 -0800

    some problems solved

--- dp_problems/maximum_alternating_subsequence.py
+++ dp_problems/maximum_alternating_subsequence.py
@@ -1,23 +1,18 @@
 #group the array as a collection of +ve -ve +ve -ve
 #for each group add the maxumun 
 
-# 1 2 -3 -5 3 -7 --- [1,2,3] [-3,-5], [3], [-7]
+# 1 2 -3 -5 3 -7 --- [1,2] [-3,-5], [3], [-7]
 
 #3,-3,3,-7
 
 def max_alternating_subseq(arr: list, n):
-    groups = []
-    left = 0
-    right = 0
+    count, left, right = 0,0,0
     while right < n:
         if not arr[right]/arr[left] > 0:
-            groups.append(arr[left:right])
+            count += max(arr[left:right])
             left = right
         right += 1
-    groups.append(arr[left:right])
-    count = 0
-    for group in groups:
-        count += max(group)
+    count += max(arr[left:right])
     return count
 
 tc = int(input())
