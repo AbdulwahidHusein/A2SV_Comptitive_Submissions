bf287746fe84ec93a83713e004eace888753a9c3
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2023-12-20 06:56:43 -0800

    Added solution for Multiply Strings

---
+++ Inperson/onboarding/multiply-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        carry1 = 1
+        carry2 = 1
+        mul = 0
+        for i in num1[::-1]:
+            carry2 = 1
+            for j in num2[::-1]:
+                mul += int(i)*carry1*int(j)*carry2
+                carry2*=10
+            carry1*=10
+        return str(mul)
+
+        
