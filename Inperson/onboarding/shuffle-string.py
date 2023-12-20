eec8029ebc3abd9cc219b62c0f3a6fae685a2248
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2023-12-20 08:42:26 -0800

    Added solution for Shuffle String

---
+++ Inperson/onboarding/shuffle-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        n = len(s)
+        ans = [0]*n
+        for i in range(n):
+            ans[indices[i]] = s[i]
+        return "".join(ans)
