b2e283db71ac2a7629c5e76e9c72e02a5944976e
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-01-02 06:26:21 -0800

    Added solution for Move Zeroes

---
+++ Inperson/onboarding/move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left = 0
+        right = 0
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left] = nums[right]
+                left += 1
+            right+=1
+        while left < len(nums):
+            nums[left] = 0
+            left += 1
+        
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
