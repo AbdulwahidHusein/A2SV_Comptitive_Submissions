ed53c4ab246a5e8d10b8d09d28ebfc7cedc19e5c
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-03-02 02:06:06 -0800

    Add solution for N-Queens

---
+++ onboarding/leetcode/n-queens.py
@@ -0,0 +1,53 @@
+class Solution:
+    def check_placement(self, board, row, col):
+        def check_diagonal(row, col, board):
+            n = len(board)
+            i, j = row, col
+            while i > 0 and j > 0:
+                i -= 1
+                j -= 1
+                if board[i][j] == "Q":
+                    return False
+            
+            i, j = row, col
+            while i < n - 1 and j < n - 1:
+                i += 1
+                j += 1
+                if board[i][j] == "Q":
+                    return False
+            i, j = row, col
+            while i < n - 1 and j > 0:
+                i += 1
+                j -= 1
+                if board[i][j] == "Q":
+                    return False
+            
+            i, j = row, col
+            while i > 0 and j < n - 1:
+                i -= 1
+                j += 1
+                if board[i][j] == "Q":
+                    return False 
+            return True 
+        n = len(board)
+        for i in range(n):
+            if board[i][col] == "Q" or board[row][i] == "Q":
+                return False
+        return check_diagonal(row, col, board)
+    
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["." for _ in range(n)] for _ in range(n)]
+        ans = []
+        
+        def solve(row):
+            if row == n:
+                ans.append(["".join(row) for row in board])
+                return
+            for col in range(n):
+                if self.check_placement(board, row, col):
+                    board[row][col] = "Q"
+                    solve(row+1)
+                    board[row][col] = "."
+        
+        solve(0)
+        return ans
