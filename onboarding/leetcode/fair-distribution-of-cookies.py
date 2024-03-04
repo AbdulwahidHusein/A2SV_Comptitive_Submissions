559caaf4056c734fac2aa541918ea3726c83da9c
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-03-04 05:37:06 -0800

    Add solution for Fair Distribution of Cookies

--- onboarding/leetcode/fair-distribution-of-cookies.py
+++ onboarding/leetcode/fair-distribution-of-cookies.py
@@ -12,14 +12,13 @@ class Solution:
                 ans = min(ans, max(dist))
                 return
 
-            for i in range(start, len(cookies)):
-                for j in range(k):
-                    if dist[j] < max_perk:
-                        dist[j] += cookies[i]
-                        _sum += cookies[i]
-                        dfs(dist, i+1, _sum)
-                        dist[j] -= cookies[i]
-                        _sum -= cookies[i]
+            for j in range(k):
+                if dist[j] < max_perk:
+                    dist[j] += cookies[start]
+                    _sum += cookies[start]
+                    dfs(dist, start+1, _sum)
+                    dist[j] -= cookies[start]
+                    _sum -= cookies[start]
         dist = [0] * k
         dfs(dist, 0, 0)
         return ans
