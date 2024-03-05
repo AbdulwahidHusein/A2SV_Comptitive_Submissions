95f3027afb204f860d9cbc65d3294c6f061841e6
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-03-05 03:21:29 -0800

    Add solution for Length of the Longest Valid Substring

--- onboarding/leetcode/length-of-the-longest-valid-substring.py
+++ onboarding/leetcode/length-of-the-longest-valid-substring.py
@@ -8,16 +8,15 @@ class Solution:
             if window <= n:
                 left = 0
                 right = window
-
-                ch = [w for w in word[:window]]
+                q = deque([w for w in word[:window]])
                 while right < n:
-                    if "".join(ch) in fset:
+                    if "".join(q) in fset:
                         end[right-1] = max( end[right-1],left)
-                    ch.pop(0)
-                    ch.append(word[right])
+                    q.popleft()
+                    q.append(word[right])
                     left += 1
                     right += 1
-                if "".join(ch) in fset:
+                if "".join(q) in fset:
                     end[right-1] = max(end[right-1], left)
         ans = 0
         prev = 0
