d5c2da9243626871733920e50fdcb86cb455bd55
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-03-06 07:26:30 -0800

    Add solution for Time Based Key-Value Store

---
+++ onboarding/leetcode/time-based-key-value-store.py
@@ -0,0 +1,23 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        candidates = self.store[key]
+        n = len(candidates)
+        lidx = bisect_left(candidates, timestamp, key = lambda x: x[0])
+        ridx = bisect_right(candidates, timestamp, key = lambda x: x[0])
+        if ridx == 0:
+            return ""
+        return candidates[ridx-1][1]
+    
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
