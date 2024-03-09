458fafadb34a9a423ba135204619013e2c02eac5
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-03-09 09:08:17 -0800

    Add solution for Trapping Rain Water

--- onboarding/leetcode/trapping-rain-water.py
+++ onboarding/leetcode/trapping-rain-water.py
@@ -3,28 +3,16 @@ class Solution:
         n = len(height)
         max_left = [0 for i in range(n)]
         max_right = [0 for i in range(n)]
-        stack = []
-        for i in range(n):
-            while stack and height[stack[-1]] < height[i]:
-                inx = stack.pop()
-                max_right[inx] = height[i]
-            stack.append(i)
-        stack2 = []
-        for i in range(n-1, -1, -1):
-            while stack and height[stack[-1]] < height[i]:
-                inx = stack.pop()
-                max_left[inx] = height[i]
-            stack.append(i)
         max_so_far = 0
         for i in range(n-1, -1, -1):
-            if max_right[i] < max_so_far:
+            if height[i] < max_so_far:
                 max_right[i] = max_so_far
-            max_so_far = max(max_so_far, max_right[i])
+            max_so_far = max(max_so_far, height[i])
         max_so_far = 0
         for i in range(n):
-            if max_left[i] < max_so_far:
+            if height[i] < max_so_far:
                 max_left[i] = max_so_far
-            max_so_far = max(max_so_far, max_left[i])
+            max_so_far = max(max_so_far, height[i])
 
         ans = 0
         for i in range(n):
