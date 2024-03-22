fac74c670dd70ff037fe35d183c6155fb60cc246
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-03-22 06:40:57 -0700

    Add solution for Missing Number

--- onboarding/leetcode/missing-number.py
+++ onboarding/leetcode/missing-number.py
@@ -1,6 +1,24 @@
 class Solution:
+    def cycleSort(self, arr):
+        for i in range(len(arr)):
+            m = arr[i] 
+            while m != i and arr[m] != m:
+                arr[i], arr[m] = arr[m], m
+                m = arr[i]
+        return arr
     def missingNumber(self, nums: List[int]) -> int:
-        s = sum(nums)
-        m = len(nums)
-
-        return m*(m+1)//2 - s
+        n = len(nums)
+        if n == 1:
+            return int(not nums[0])
+        for i in range(n):
+            if nums[i] == n:
+                if i > 0:
+                    nums[i] = nums[i-1]
+                else:
+                    nums[i] = nums[i+1]
+        s = self.cycleSort(nums)
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+        return n
+         
