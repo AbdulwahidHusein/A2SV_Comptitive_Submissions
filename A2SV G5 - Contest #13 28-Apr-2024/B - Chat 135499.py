c1600e8643763b37af9ec9e6df87b812b02c64f8
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-04-28 05:00:49 -0700

    Created 'B - Chat' by A2SV Hub

---
+++ A2SV G5 - Contest #13 28-Apr-2024/B - Chat 135499.py
@@ -0,0 +1,24 @@
+# Problem: B - Chat - https://codeforces.com/gym/519135/problem/B
+
+from heapq import *
+n,k, q = map(int, input().split())
+t = list(map(int, input().split()))
+onlines = set()
+disp = []
+
+
+for i in range(q):
+    typ, id = map(int, input().split())
+    if typ == 2:
+        if not id in onlines:
+            print("NO")
+        elif disp[0] > t[id-1]:
+            print("NO")
+        else:
+            print("YES")
+    else:
+        onlines.add(id)
+        if len(disp) >= k and disp[0] < t[id-1]:
+            heapreplace(disp, t[id-1])
+        elif len(disp) < k:
+            heappush(disp, t[id-1])
