e0e71d2371738daf469566574673486a53fe7b91
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-04-28 05:00:55 -0700

    Created 'A - Balanced Alphabet' by A2SV Hub

---
+++ A2SV G5 - Contest #13 28-Apr-2024/A - Balanced Alphabet 135500.py
@@ -0,0 +1,10 @@
+# Problem: A - Balanced Alphabet - https://codeforces.com/gym/519135/problem/A
+
+for _ in range(int(input())):
+    n,k = map(int, input().split())
+    res = []
+    a = 0
+    for i in range(n):
+        res.append(chr(ord("a")+a%k))
+        a += 1
+    print("".join(res))
