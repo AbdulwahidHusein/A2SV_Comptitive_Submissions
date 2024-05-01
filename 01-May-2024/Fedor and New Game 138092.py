6969e0e4291bb5c534d0e6d2fedc306bb5b65390
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-05-01 01:13:45 -0700

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 01-May-2024/Fedor and New Game 138092.py
@@ -0,0 +1,20 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n,m, k = map(int, input().split())
+armies = []
+for _ in range(m+1):
+    armies.append(int(input()))
+
+fedor = armies.pop()
+freinds = 0
+for army in armies:
+    xor = army ^ fedor
+    count = 0
+    while xor:
+        if xor & 1:
+            count += 1
+        xor >>= 1
+    if count <= k:
+        freinds += 1
+        
+print(freinds)
