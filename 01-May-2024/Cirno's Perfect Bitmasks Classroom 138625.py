90eec364b317e476c958fcc7b5ad52da7826776c
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-05-01 11:01:22 -0700

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 01-May-2024/Cirno's Perfect Bitmasks Classroom 138625.py
@@ -0,0 +1,21 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+
+for _ in range(int(input())):
+    n = int(input())
+    i = 0
+    if n == 1:
+        print(3)
+    else:
+        while n and 1<<i &n == 0:
+            i+=1
+        ans = 1 << i
+        bits  = 0
+        for j in range(32):
+            if 1 << j & n:
+                bits += 1
+                
+        if bits == 1:
+            ans += 1
+        print(ans)
+    
