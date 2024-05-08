07e7ff9d4e505b59a4a51cfa7f8a8dbdb320a41c
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-05-08 00:04:25 -0700

    Created 'A - a-e-i-o-u-y' by A2SV Hub

---
+++ A2SV G5 - Contest #15 08-May-2024/A - a-e-i-o-u-y 143071.py
@@ -0,0 +1,15 @@
+# Problem: A - a-e-i-o-u-y - https://codeforces.com/gym/522079/problem/A
+
+l = int(input())
+vow = ["a", 'e','i',  'o','u','y']
+w = input().strip()
+s = []
+for a in w:
+    if s:
+        if a in vow and s[-1] in vow:
+            continue
+        
+    s.append(a)
+    
+s = "".join(s)
+print(s)
