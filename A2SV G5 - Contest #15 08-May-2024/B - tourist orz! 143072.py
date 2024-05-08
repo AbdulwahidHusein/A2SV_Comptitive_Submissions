a7cc349610ae61cf6f2673ae22d1ae25286fa360
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-05-08 00:04:33 -0700

    Created 'B - tourist orz!' by A2SV Hub

---
+++ A2SV G5 - Contest #15 08-May-2024/B - tourist orz! 143072.py
@@ -0,0 +1,10 @@
+# Problem: B - tourist orz! - https://codeforces.com/gym/522079/problem/B
+
+for _ in range(int(input())):
+    n, z = map(int,input().split())
+    nums = list(map(int, input().split()))
+    ans = 0
+    for m in nums:
+        ans = max(ans, z |m)
+        
+    print(ans)
