91c70bc1e9ad199dbe8e9f3c7527b0cec9ba789c
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-05-08 00:04:41 -0700

    Created 'C - ANDy Session' by A2SV Hub

---
+++ A2SV G5 - Contest #15 08-May-2024/C - ANDy Session 143073.py
@@ -0,0 +1,27 @@
+# Problem: C - ANDy Session - https://codeforces.com/gym/522079/problem/C
+
+def solve():
+    n, k = map(int, input().split())
+    nums = [int(i) for i in input().split()]
+    ones = [0] * 32
+    for m in nums:
+        for i in range(32):
+            if m & (1 << i):
+                ones[i] += 1
+
+    full = []
+    for i in range(30, -1, -1):
+        if ones[i] < n and k >= (n - ones[i]):
+                k -= (n - ones[i])
+                full.append(i)
+                
+    ans = nums[0]
+    for m in nums:
+        ans &= m
+    for pos in full:
+        ans |= (1 << (pos))
+    print(ans)
+
+
+for _ in range(int(input())):
+    solve()
