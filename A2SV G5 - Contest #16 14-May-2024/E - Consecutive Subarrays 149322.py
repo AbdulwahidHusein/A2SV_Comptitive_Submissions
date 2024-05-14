2d03e961e0b888c0ef3239e7ed90c826bfeeb245
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-05-14 07:14:05 -0700

    Created 'E - Consecutive Subarrays' by A2SV Hub

---
+++ A2SV G5 - Contest #16 14-May-2024/E - Consecutive Subarrays 149322.py
@@ -0,0 +1,20 @@
+# Problem: E - Consecutive Subarrays - https://codeforces.com/gym/523525/problem/E
+
+n, k = map(int, input().split())
+nums = [int(i) for i in input().split()]
+
+ps = []
+for i in range(n-1, -1, -1):
+    if ps:
+        ps.append(ps[-1] + nums[i])
+    else:
+        ps = [nums[i]]
+    
+
+ans = ps.pop()
+ps.sort()
+for i in range(k-1):
+    ans += ps.pop()
+    
+print(ans)
+
