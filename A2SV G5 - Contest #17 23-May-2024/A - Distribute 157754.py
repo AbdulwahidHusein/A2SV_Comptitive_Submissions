a9fc50a151438a5e6df9bf1934bfb59483782e25
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-05-23 03:18:51 -0700

    Created 'A - Distribute' by A2SV Hub

---
+++ A2SV G5 - Contest #17 23-May-2024/A - Distribute 157754.py
@@ -0,0 +1,11 @@
+# Problem: A - Distribute - https://codeforces.com/gym/524965/problem/A
+
+n = int(input())
+
+nums = [int(i) for i in input().split()]
+for i in range(n):
+    nums[i] = (nums[i], i)
+nums.sort()
+
+for i in range(n//2):
+    print(nums[i][1]+1, nums[n-i-1][1]+1)
