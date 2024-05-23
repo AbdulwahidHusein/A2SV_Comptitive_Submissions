0ebf9c173e37a2b3f347958fbf9b4370170b7c7f
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-05-23 03:18:39 -0700

    Created 'C - Palindromic Sum Representations' by A2SV Hub

---
+++ A2SV G5 - Contest #17 23-May-2024/C - Palindromic Sum Representations 157752.py
@@ -0,0 +1,23 @@
+# Problem: C - Palindromic Sum Representations - https://codeforces.com/gym/524965/problem/C
+
+def is_pas(n):
+    return str(n) == str(n)[::-1]
+m = 40001
+mod = 10 ** 9 + 7
+dp = [0 for _ in range(m)]
+dp[0] = 1
+
+for i in range(1, m):
+    if str(i) == str(i)[::-1]:
+        for j in range(1, m):
+            if j >= i:
+                dp[j] += dp[j - i]
+                dp[j] %= mod
+            
+
+def solve():
+    n = int(input())
+    print(dp[n] %mod)
+    
+for _ in range(int(input())):
+    solve()
