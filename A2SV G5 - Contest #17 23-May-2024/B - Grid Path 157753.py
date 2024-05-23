19e04c979b063dce60f78f7e83cca03d1cd3e284
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-05-23 03:18:45 -0700

    Created 'B - Grid Path' by A2SV Hub

---
+++ A2SV G5 - Contest #17 23-May-2024/B - Grid Path 157753.py
@@ -0,0 +1,9 @@
+# Problem: B - Grid Path - https://codeforces.com/gym/524965/problem/B
+
+for _ in range(int(input())):
+    n,m, k = map(int, input().split())
+    
+    if m - 1 + m * (n-1) == k:
+        print("YES")
+    else:
+        print("NO")
