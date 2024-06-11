b1e6aef040d2a8d7e8ed15cb4dbb439b5de0c613
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-06-10 22:33:25 -0700

    Created 'First Missing Positive' by A2SV Hub

---
+++ 11-Jun-2024/First Missing Positive 166665.py
@@ -0,0 +1,23 @@
+# Problem: First Missing Positive - https://leetcode.com/problems/first-missing-positive/description/
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        exist = n + 1
+        for i in range(n):
+            if nums[i] > 0 and nums[i] <= n:
+                exist = nums[i]
+        if exist == n+1:
+            return 1
+        for i in range(n):
+            if nums[i] > n or nums[i] <= 0:
+                nums[i] = exist
+        for i in range(n):
+            r = nums[i]
+            while r != i+1 and nums[r-1] != r:
+                nums[i], nums[r-1] = nums[r-1], nums[i]
+                r = nums[i]
+        for i in range(n):
+            if nums[i] != i+1:
+                return i+1
+        return n + 1
