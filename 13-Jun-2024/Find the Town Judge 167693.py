6680c4aca53b236d59484bc9153c7ae8921a8260
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-06-13 04:16:00 -0700

    Created 'Find the Town Judge' by A2SV Hub

---
+++ 13-Jun-2024/Find the Town Judge 167693.py
@@ -0,0 +1,17 @@
+# Problem: Find the Town Judge - https://leetcode.com/problems/find-the-town-judge/
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        if trust == [] and  n == 1:
+            return 1
+        trusted_c = defaultdict(int)
+        trusts_c = defaultdict(int)
+
+        for a, b in trust:
+            trusts_c[a] += 1
+            trusted_c[b] += 1
+        
+        for m in trusted_c:
+            if trusted_c[m] == n-1 and trusts_c[m] == 0:
+                return m
+        return -1
