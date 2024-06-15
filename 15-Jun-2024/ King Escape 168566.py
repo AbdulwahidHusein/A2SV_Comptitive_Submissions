f114565a253e4071979146fadf1127ae6ded0385
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-06-15 00:15:12 -0700

    Created ' King Escape' by A2SV Hub

---
+++ 15-Jun-2024/ King Escape 168566.py
@@ -0,0 +1,14 @@
+# Problem:  King Escape - https://codeforces.com/problemset/problem/1033/A
+
+n = int(input())
+x, y = map(int, input().split())
+a, b = map(int, input().split())
+c,d = map(int, input().split())
+
+coord1 = int(x > a) + 2*(int(y > b))
+coord2 = int(x > c)  + 2*(int(y > d))
+
+if coord1 == coord2:
+    print("YES")
+else:
+    print("NO")
