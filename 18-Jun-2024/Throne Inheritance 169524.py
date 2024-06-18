188ca66c248ae9f381fe9a7a0be7cb16ec60a8a1
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-06-18 05:04:52 -0700

    Created 'Throne Inheritance' by A2SV Hub

---
+++ 18-Jun-2024/Throne Inheritance 169524.py
@@ -0,0 +1,50 @@
+# Problem: Throne Inheritance - https://leetcode.com/problems/throne-inheritance/
+
+class ThroneNode:
+    def __init__(self, name):
+        self.name = name
+        self.childs = []
+
+class ThroneInheritance:
+    def __init__(self, kingName: str):
+        self.kingNode = ThroneNode(kingName)
+        self.childArray = {}
+        self.childArray[kingName] = self.kingNode.childs
+
+        self.isDead = set()
+
+    def birth(self, parentName: str, childName: str) -> None:
+        newNode = ThroneNode(childName)
+        self.childArray[parentName].append(newNode)
+        self.childArray[childName] = newNode.childs
+
+    def death(self, name: str) -> None:
+        self.isDead.add(name)
+
+    def getInheritanceOrder(self) -> List[str]:
+        path = []
+        stack = [self.kingNode]
+        while stack:
+            node = stack.pop()
+            if node.name not in self.isDead:
+                path.append(node.name)
+            for child in reversed(node.childs): 
+                stack.append(child)
+        return path
+
+        # # def dfs(node):
+        # #     if not node:
+        # #         return
+        # #     if node.name not in self.isDead:
+        # #         path.append(node.name)
+        # #     for child in node.childs:
+        # #         dfs(child)
+        # # dfs(self.kingNode)
+        # return path
+
+
+# Your ThroneInheritance object will be instantiated and called as such:
+# obj = ThroneInheritance(kingName)
+# obj.birth(parentName,childName)
+# obj.death(name)
+# param_3 = obj.getInheritanceOrder()
