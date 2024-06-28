70306542c6213d6092c1e6d3d9fe1532069f83ab
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2024-06-28 09:46:09 -0700

    Created 'String Comperession' by A2SV Hub

---
+++ 28-Jun-2024/String Comperession 174074.py
@@ -0,0 +1,17 @@
+# Problem: String Comperession - https://leetcode.com/problems/string-compression/
+
+class Solution:
+    def compress(self, cs: List[str]) -> int:
+        idx1 , idx2 = 0, 0
+        while idx1 < len(cs):
+            c, start = cs[idx1],idx1
+            while idx1<len(cs) and cs[idx1] == c:
+                idx1+=1
+            cs[idx2]= c
+            idx2+=1
+            count=idx1-start
+            if count>1:
+                for digit in str(count):
+                    cs[idx2]=digit
+                    idx2+=1
+        return idx2 
